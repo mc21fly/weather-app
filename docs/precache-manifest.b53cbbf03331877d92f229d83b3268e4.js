c781e98240d3aa6814b3479b27443dc5637c55e2
Michał <michalbkl@poczta.fm>
2020-05-20 10:43:29 +0200

    build

---
+++ docs/precache-manifest.b53cbbf03331877d92f229d83b3268e4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6d3efbece0d478586918ad0388e65c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d04f5b27bf252f9e6006",
+    "url": "/weather-app/static/css/2.c5eb936e.chunk.css"
+  },
+  {
+    "revision": "11a54bfc500f8ae96b8b",
+    "url": "/weather-app/static/css/main.06e22c18.chunk.css"
+  },
+  {
+    "revision": "d04f5b27bf252f9e6006",
+    "url": "/weather-app/static/js/2.ffc42f0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.ffc42f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a54bfc500f8ae96b8b",
+    "url": "/weather-app/static/js/main.a9ee9a3a.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.823dee738269900bbbecb19c5bb55653.js"
+  "/weather-app/precache-manifest.b53cbbf03331877d92f229d83b3268e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
