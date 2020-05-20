6d94a2ef7dc757a3278b224a6e39f5099cb5b3f9
Michał <michalbkl@poczta.fm>
2020-05-20 10:28:39 +0200

    build

---
+++ docs/precache-manifest.823dee738269900bbbecb19c5bb55653.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49cae342dd16076862e4c21ff2729af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "109573db777cf93cd508",
+    "url": "/static/css/2.2b6121de.chunk.css"
+  },
+  {
+    "revision": "11a54bfc500f8ae96b8b",
+    "url": "/static/css/main.06e22c18.chunk.css"
+  },
+  {
+    "revision": "109573db777cf93cd508",
+    "url": "/static/js/2.ffc42f0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ffc42f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a54bfc500f8ae96b8b",
+    "url": "/static/js/main.a9ee9a3a.chunk.js"
+  },
+  {
+    "revision": "5855bf49084dc0464ab6",
+    "url": "/static/js/runtime-main.b71c37de.js"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
